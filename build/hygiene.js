8595c948dcee2e25a4da8b1cfde23bc060d485b4
samuelmaddock <samuel.maddock@gmail.com>
2020-11-25 23:18:19 -0500

    chore: restore extension gallery

--- build/hygiene.js
+++ build/hygiene.js
@@ -167,12 +167,12 @@ function hygiene(some) {
 	let errorCount = 0;
 
 	const productJson = es.through(function (file) {
-		const product = JSON.parse(file.contents.toString('utf8'));
+		// const product = JSON.parse(file.contents.toString('utf8'));
 
-		if (product.extensionsGallery) {
-			console.error(`product.json: Contains 'extensionsGallery'`);
-			errorCount++;
-		}
+		// if (product.extensionsGallery) {
+		// 	console.error(`product.json: Contains 'extensionsGallery'`);
+		// 	errorCount++;
+		// }
 
 		this.emit('data', file);
 	});
